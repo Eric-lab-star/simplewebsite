543971baabe4cb86c9577faccc3baa2ff956afd2
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-12-04 17:33:02 +0900

    login screen

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,11 +1,1 @@
 import "../scss/styles.scss";
-
-const nav = document.querySelector(".nav");
-const list = document.querySelector("#list");
-
-const extendNav = () => {
-  nav.classList.toggle("extended_nav");
-  nav.classList.toggle("nav");
-};
-
-list.addEventListener("click", extendNav);

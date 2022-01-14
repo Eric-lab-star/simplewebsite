2be42930075ccdfd435bf3b63135905bd3f4a8f3
Eric <cyon256@icloud.com>
2022-01-14 19:14:01 +0900

    fix handleSound

--- src/client/js/video.js
+++ src/client/js/video.js
@@ -4,6 +4,13 @@ const muteBtn = document.querySelector(".muteBtn");
 const soundBar = document.querySelector(".soundBar");
 const currentTime = document.querySelector("#currentTime");
 const totalTime = document.querySelector("#totalTime");
+const fullScreenBtn = document.querySelector(".fullScreenBtn");
+const videoContainer = document.querySelector(".watchVideo");
+const controls = document.querySelector("controls");
+let volumeLevel;
+
+video.volume = soundBar.value;
+//play
 const handlePlayBtn = () => {
   if (video.paused === true) {
     video.play();
@@ -13,25 +20,35 @@ const handlePlayBtn = () => {
     playBtn.innerText = "Play";
   }
 };
+
+//sound
 const handleMuteBtn = () => {
   if (video.muted) {
     muteBtn.innerText = "Mute";
+    soundBar.value = volumeLevel;
+
     video.muted = false;
     return;
   } else {
     muteBtn.innerText = "Unmute";
+
+    soundBar.value = 0;
+
     video.muted = true;
     return;
   }
 };
+
 const handleSound = () => {
   if (video.muted) {
     video.muted = false;
     muteBtn.innerText = "Mute";
   }
+  volumeLevel = soundBar.value;
   video.volume = soundBar.value;
 };
 
+//time range
 const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substring(14, 19);
 
@@ -45,9 +62,28 @@ const handleTime = () => {
   currentTime.innerText = formatTime(Math.floor(video.currentTime));
   return;
 };
+//full screen
+function handleFullScreen() {
+  const fullScreen = document.fullscreenElement;
+  if (!fullScreen) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+function handleMousemove() {
+  video.classList.add("showing");
+}
+
+function handleMouseLeave() {
+  setTimeout(() => video.classList.remove("showing"), 30000);
+}
 
 playBtn.addEventListener("click", handlePlayBtn);
 muteBtn.addEventListener("click", handleMuteBtn);
 soundBar.addEventListener("input", handleSound);
 video.addEventListener("loadedmetadata", handleDuration);
 video.addEventListener("timeupdate", handleTime);
+fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMousemove);
